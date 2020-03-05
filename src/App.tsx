3b34fc434b7db35d3bb0c9ca0363ac2df1ed2357
maruware <me@maruware.com>
2020-03-06 02:15:42 +0900

    add usage

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react'
 import { setupAudio, Audio } from './audio'
 import { rotate } from './geometry'
 
+import styled from 'styled-components'
+
 interface Point {
   x: number
   y: number
@@ -16,13 +18,21 @@ interface Status {
 const WIDTH = 500
 const HEIGHT = 500
 
+const Row = styled.div`
+  display: flex;
+`
+
+const SvgContainer = styled.div`
+  margin-right: 8px;
+`
+
 function App() {
   const [me, setMe] = useState<Status>({
     pos: { x: WIDTH / 2, y: HEIGHT / 2 },
     rotation: 0
   })
 
-  const [speaker, setSpeaker] = useState<Point>({ x: WIDTH / 2, y: 0 })
+  const [speaker] = useState<Point>({ x: WIDTH / 2, y: 0 })
 
   const [audio, setAudio] = useState<Audio>()
   useEffect(() => {
@@ -90,27 +100,37 @@ function App() {
         <button onClick={handleStop}>Stop</button>
       </div>
 
-      <svg viewBox={`0, 0, ${WIDTH}, ${HEIGHT}`} style={{ width: 700 }}>
-        <rect
-          x={0}
-          y={0}
-          width={WIDTH}
-          height={HEIGHT}
-          stroke="#555"
-          strokeWidth={1}
-          fillOpacity={0}
-        />
-        <g
-          transform={`translate(${me.pos.x}, ${me.pos.y}) rotate(${me.rotation})`}
-        >
-          <polygon points="-4,-6 0,-12 4,-6" fill="black" />
-          <circle cx={0} cy={0} r={4} fill="black" />
-        </g>
+      <Row>
+        <SvgContainer>
+          <svg viewBox={`0, 0, ${WIDTH}, ${HEIGHT}`} style={{ width: 700 }}>
+            <rect
+              x={0}
+              y={0}
+              width={WIDTH}
+              height={HEIGHT}
+              stroke="#555"
+              strokeWidth={1}
+              fillOpacity={0}
+            />
+            <g
+              transform={`translate(${me.pos.x}, ${me.pos.y}) rotate(${me.rotation})`}
+            >
+              <polygon points="-4,-6 0,-12 4,-6" fill="black" />
+              <circle cx={0} cy={0} r={4} fill="black" />
+            </g>
 
-        <g transform={`translate(${speaker.x}, ${speaker.y})`}>
-          <rect x={-10} y={-5} width={20} height={10} />
-        </g>
-      </svg>
+            <g transform={`translate(${speaker.x}, ${speaker.y})`}>
+              <rect x={-10} y={-5} width={20} height={10} />
+            </g>
+          </svg>
+        </SvgContainer>
+
+        <div>
+          <h3>Usage</h3>
+          <p>Move: Left, Right, Top, Botton</p>
+          <p>Rotate: Z, X</p>
+        </div>
+      </Row>
     </div>
   )
 }
